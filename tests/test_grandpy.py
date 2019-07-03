fa1fa651d3fc34c6a24fd3c5bae6c332f0c15fd4
pascal MACCHI <pascal.macchi@gmail.com>
2019-07-03 23:47:27 +0200

    Session_03-07-2019:Probleme importation sur fichier test_grandpy.py

---
+++ tests/test_grandpy.py
@@ -0,0 +1,19 @@
+#parser()
+
+import unittest
+import require 
+Parser = require('../python') 
+"""separer la chaine de caractere en plusieurs mots"""
+
+
+class TestMot(unittest.TestCase):
+    """Test du retour de la demande d'adresse"""
+    
+    def test_demande(self):
+        """ test de la demande """ 
+        demande = "ou est situé le restaurant la_nappe_d\'or"
+        result = parser(demande)
+        self.assertIn(result, "lyon")
+
+if __name__ == "__main__":
+    unittest.main()
