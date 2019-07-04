84cbde54f31041519f0a3001b21d20eae66fd2c7
pascal MACCHI <pascal.macchi@gmail.com>
2019-07-04 22:20:33 +0200

    modification importation du fichier 'parser' ligne 4 du fichier test_g.py

--- test_g.py
+++ test_g.py
@@ -1,7 +1,7 @@
 import unittest
-import require
-parser = require('/python')
-# ~ from python.grandPy import parser as p
+# ~ import require
+# ~ parser = require('/python')
+from python.grandPy import parser as p
 """separer la chaine de caractere en plusieurs mots"""
 
 
@@ -11,8 +11,9 @@ class TestMot(unittest.TestCase):
     def test_demande(self):
         """ test de la demande """
         demande = "ou est situé le restaurant la_nappe_d\'or"
-        result = parser(demande)
+        result = p.parser(demande)
         self.assertIn(result, "lyon")
 
 if __name__ == "__main__":
     unittest.main()
+
