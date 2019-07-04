2bb3a9ae55c21ec758d6c3f69b3f9a00af6312e6
pascal MACCHI <pascal.macchi@gmail.com>
2019-07-04 18:53:11 +0200

    Ajouter fichier test_g.py (racine projet)

---
+++ test_g.py
@@ -0,0 +1,18 @@
+import unittest
+import require
+parser = require('/python')
+# ~ from python.grandPy import parser as p
+"""separer la chaine de caractere en plusieurs mots"""
+
+
+class TestMot(unittest.TestCase):
+    """Test du retour de la demande d'adresse"""
+
+    def test_demande(self):
+        """ test de la demande """
+        demande = "ou est situé le restaurant la_nappe_d\'or"
+        result = parser(demande)
+        self.assertIn(result, "lyon")
+
+if __name__ == "__main__":
+    unittest.main()
