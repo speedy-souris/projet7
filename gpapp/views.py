8d13198292f5708e14da4a6c47681086b0b65227
pascal MACCHI <pascal.macchi@gmail.com>
2019-07-16 18:16:27 +0200

    Mise en place template base.html

--- gpapp/views.py
+++ gpapp/views.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
+
 @app.route('/')
-@app.route('/index/')
 def index():
     return render_template('index.html')
 
