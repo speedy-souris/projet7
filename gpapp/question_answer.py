bf9fa6da1402f5ffb7fccf8ed6a7f87b803e26c2
Pascal MACCHI <pascal.macchi@gmail.com>
2019-08-08 10:31:45 +0200

    Suppression de la configuration GrandPy renomer projet en un seul fichier question_answer (code source et fichier test)

--- gpapp/question_answer.py
+++ gpapp/question_answer.py
@@ -2,9 +2,81 @@
 import json
 import urllib.request, urllib.parse
 
-
 from config import key_API_MAP, address_default
 from config import placeId_default
+
+# parser
+def parser(question):
+    list_question = question.split()
+    unnecessary = ["a","abord","absolument","afin","ah","ai","aie","ailleurs","ainsi","ait",
+                    "allaient","allo","allons","allô","alors","ancetre","anterieur","anterieure",
+                    "anterieures","apres","après","as","assez","attendu","au","aucun",
+                    "aucune","aujourd","aujourd'hui","aupres","auquel","aura","auraient",
+                    "aurait","auront","aussi","autre","autrefois","autrement","autres",
+                    "autrui","aux","auxquelles","auxquels","avaient","avais","avait","avant",
+                    "avec","avoir","avons","ayant","b","bah","bas","basee","bat","beau",
+                    "beaucoup","bien","bigre","boum","bravo","brrr","c","car","ce","ceci",
+                    "cela","celle","celle-ci","celle-là","celles","celles-ci","celles-là",
+                    "celui","celui-ci","celui-là","cent","cependant","certain","certaine",
+                    "certaines","certains","certes","ces","cet","cette","ceux","ceux-ci",
+                    "ceux-là","chacun","chacune","chaque","cher","chers","chez","chiche",
+                    "chut","chère","chères","ci","cinq","cinquantaine","cinquante",
+                    "cinquantième","cinquième","clac","clic","combien","comme","comment",
+                    "comparable","comparables","compris","concernant","contre","couic",
+                    "crac","d","da","dans","de","debout","dedans","dehors","deja","delà",
+                    "depuis","dernier","derniere","derriere","derrière","des","desormais",
+                    "desquelles","desquels","dessous","dessus","deux","deuxième","deuxièmement",
+                    "devant","devers","devra","different","differentes","differents",
+                    "différent","différente","différentes","différents","dire","directe",
+                    "directement","dit","dite","dits","divers","diverse","diverses","dix",
+                    "e","effet","egale","egalement","egales","eh","elle","elle-même","elles",
+                    "elles-mêmes","en","encore","enfin","entre","envers","environ","es","est",
+                    "et","etant","etc","etre","eu","euh","eux","eux-mêmes","exactement",
+                    "excepté","extenso","exterieur","f","fais","faisaient","faisant","fait",
+                    "façon","feront","fi","flac","floc","font","g","gens","h","ha","hein","hem",
+                    "hep","hey","hi","ho","holà","hop","hormis","hors","hou","houp","hue","hui","huit",
+                    "huitième","hum","hurrah","hé","hélas","i","il","ils","importe","j","je",
+                    "jusqu","jusque","juste","k","l","la","laisser","laquelle","las","le",
+                    "lequel","les","lesquelles","lesquels","leur","leurs","longtemps","lors",
+                    "lorsque","lui","lui-meme","lui-même","là","lès","m","ma","maint","maintenant",
+                    "mais","malgre","malgré","maximale","me","meme","memes","merci","mes","mien",
+                    "m'indiquer","m'orienter","mienne","miennes","miens","mille","mince","minimale",
+                    "moi","moi-meme","moi-même","moindres","moins","mon","moyennant","multiple","multiples",
+                    "même","mêmes","n","na","naturel","naturelle","naturelles","ne","neanmoins",
+                    "necessaire","necessairement","neuf","neuvième","ni","nombreuses","nombreux",
+                    "non","nos","notamment","notre","nous","nous-mêmes","nouveau","nul","néanmoins",
+                    "nôtre","nôtres","o","oh","ohé","ollé","olé","on","ont","onze","onzième","ore",
+                    "ou","ouf","ouias","oust","ouste","outre","ouvert","ouverte","ouverts","o|",
+                    "où","p","paf","pan","papi","papy","par","parce","parfois","parle","parlent",
+                    "parler","parmi", "parseme","partant","particulier","particulière","particulièrement","pas",
+                    "passé","pendant","pense","permet","personne","peu","peut","peuvent","peux",
+                    "pff","pfft","pfut","pif","pire","plein","plouf","plus","plusieurs","plutôt",
+                    "possessif","possessifs","possible","possibles","pouah","pour","pourquoi",
+                    "pourrais","pourrait","pouvait","prealable","precisement","premier","première",
+                    "premièrement","pres","probable","probante","procedant","proche","près","psitt",
+                    "pu","puis","puisque","pur","pure","q","qu","quand","quant","quant-à-soi",
+                    "quanta","quarante","quatorze","quatre","quatre-vingt","quatrième",
+                    "quatrièmement","que","quel","quelconque","quelle","quelles","quelqu'un","quelque",
+                    "quelques","quels","qui","quiconque","quinze","quoi","quoique","r","rare","rarement",
+                    "rares","relative","relativement","remarquable","rend","rendre","restant","reste",
+                    "restent","restrictif","retour","revoici","revoilà","rien","s","sa","sacrebleu","sait",
+                    "salut", "sans","sapristi","sauf","se","sein","seize","selon","semblable","semblaient","semble",
+                    "semblent","sent","sept","septième","sera","seraient","serait","seront","ses","seul",
+                    "seule","seulement","si","sien","sienne","siennes","siens","sinon","situe", "situé",
+                    "six","sixième","soi","soi-même","soit","soixante","son","sont","sous","souvent","specifique",
+                    "specifiques","speculatif","stop","strictement","subtiles","suffisant","suffisante",
+                    "suffit","suis","suit","suivant","suivante","suivantes","suivants","suivre","superpose",
+                    "sur","surtout","t","ta","tac","tant","tardive","te","tel","telle","tellement","telles",
+                    "tels","tenant","tend","tenir","tente","tes","tic","tien","tienne","tiennes","tiens","toc",
+                    "toi","toi-même","ton","touchant","toujours","tous","tout","toute","toutefois","toutes",
+                    "treize","trente","tres","trois","troisième","troisièmement","trop","trouve","très","tsoin","tsouin",
+                    "tu","té","u","un","une","unes","uniformement","unique","uniques","uns","v","va","vais",
+                    "vas","vers","via","vieux","vif","vifs","vingt","vivat","vive","vives","vlan","voici","voilà",
+                    "vont","vos","votre","vous","vous-mêmes","vu","vé","vôtre","vôtres","w","x","y","z","zut",
+                    "à","â","ça","ès","étaient","étais","était","étant","été","être","ô",",",";",".","?","!"]
+
+    result = [w for w in list_question if w.lower() not in unnecessary]
+    return result
 
 # place_id search on Google Map API
 def get_place_id(key=key_API_MAP, address=address_default):
@@ -38,7 +110,10 @@ def get_address(key=key_API_MAP, place_id=placeId_default):
 
 if __name__ == "__main__":
 
-    test = get_place_id()
-    print(test)
-    test2 = get_address()
-    print(test2)
+    test_parse = parser("Salut papy  peut tu m'orienter vers la poste de marseille")
+    test_placeId = get_place_id()
+    test_address = get_address()
+
+    print(test_parse)
+    print(test_placeId)
+    print(test_address)
--- gpapp/tests/test_questionAnswer.py
+++ gpapp/tests/test_questionAnswer.py
@@ -5,8 +5,21 @@ import urllib.request
 import json
 from io import BytesIO
 
+from gpapp.question_answer import parser
+from gpapp.question_answer import get_place_id, get_address
 
-from gpapp.question_answer import get_place_id
+# parser test on the question asked to grandPy
+def test_parser():
+    """
+    Test function on the separation of the character string (question asked
+     a papyRobot alias grandPy) in several words,
+     removing unnecessary words in order to keep the keywords for the
+     search (location history & geographic coordinates)
+    """
+    # question asked to grandPy
+    demand = "ou est situé le restaurant la_nappe_d_or de lyon"
+
+    assert parser(demand) == ["restaurant","la_nappe_d_or","lyon"]
 
 # google map API test on place id location
 def test_geolocal_id(monkeypatch):
@@ -56,4 +69,4 @@ def test_geolocal_address(monkeypatch):
     monkeypatch.setattr(urllib.request, 'urlopen',
         mockreturn)
 
-    assert get_place_id() == resul_address
+    assert get_address() == resul_address
