863f0a0ce794dd5cdc4a2290b04006840b1c2652
Pascal MACCHI <pascal.macchi@gmail.com>
2019-09-25 15:41:09 +0200

    ajout du favicon grandpy

--- gpapp/static/js/grandPy.js
+++ gpapp/static/js/grandPy.js
@@ -1,18 +1,26 @@
+
 $(document).ready(function(){
+    function answer_gp(response){
+        console.log('la reponse'+ response);
+        $('.home').hide();
+        $('#gp_reply').show();
+        $('.answer').show();
+        $('#gp_reflection').hide();
+    };
+    $('#submit2').submit(function(e){
 
     /*
      *send a question to GrandPy
     */
-
-    $.get(
-        'result/1',
-        'false'
-        //~ reflection
-    );
-
-
+        $('.home').hide();
+        $('#gp_reflection').show();
+        $.ajax({
+            url: '/index/3',
+            type: 'GET',
+            dataType: 'html',
+            success: answer_gp
+        });
+        e.preventDefault();
+    });
 
 });
-function reflection(){
-        windows.location="localhost:5000/result.html";
-    };
--- gpapp/views.py
+++ gpapp/views.py
@@ -8,14 +8,20 @@ from . import question_answer
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+
+    return render_template('index.html')
+
 @app.route('/index/<reflection>')
-def index(reflection):
+def answer_gp(reflection):
     question = request.args.get('question')
     # ~ politeness = ['bonjour','salut','bonsoir']
-    time_reflection = time.sleep(int(reflection))
-    return render_template('index.html',
-                                question = question, time_reflection = time_reflection)
 
+    time_reflection = time.sleep(int(reflection))
+
+    # ~ return render_template('index.html',
+                                # ~ question = question, time_reflection = time_reflection)
+    return 'blablabla'
 
 
 # ~ if __name__ == "__main__":
