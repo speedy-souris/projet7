024a6ef99bdc2c14ef2928a388c5633cff46176a
Pascal MACCHI <pascal.macchi@gmail.com>
2019-11-20 10:52:57 +0100

    mise en place gestion de quotas api

--- gpapp/static/js/grandPy.js
+++ gpapp/static/js/grandPy.js
@@ -2,25 +2,37 @@
 $(document).ready(function(){
     function answer_gp(response){
         var response_json = JSON.parse(response);
-        $("#ask").hide();
-        $("#word_of_welcome").hide();
-        $("#gp_reply").show();
-        $("#answer").show();
-        $("#gp_reflection").hide();
-        console.log(response);
-        $("#address").text("l'adresse se situe : "
-                            +JSON.stringify(response_json["address"]["result"]["formatted_address"]));
-
-        $("#map").src = response["ref_map"];
-
-        wiki_answer = response_json["history"];
-
-        if (wiki_answer[2][0]){
-            $("#history").text(JSON.stringify(wiki_answer[2][0]))
+        console.log(response_json);
+        if (response_json["quotas_api"]["over_quotas"] == "True"){
+            $("#gp_reflection").hide();
+            $("#ask").hide();
+            $("#other").hide();
+            $("#window_sill").hide();
+            $("#quotas").show();
         }
         else{
-            $("#history").text("Aie aie aie, le 'WIKI' est vide... !")
-        };
+            $("#ask").hide();
+            $("#word_of_welcome").hide();
+            $("#gp_reply").show();
+            $("#answer").show();
+            $("#gp_reflection").hide();
+            $("#other").show();
+
+            $("#address").text("l'adresse se situe : "
+                                +JSON.stringify(response_json["answer"]["address"]["result"]["formatted_address"]));
+
+            $("#map").src = response_json["display_map"];
+
+            wiki_answer = response_json["answer"]["history"];
+
+            if (wiki_answer[2][0]){
+                $("#history").text(JSON.stringify(wiki_answer[2][0]));
+            }
+            else{
+                $("#history").text("Aie aie aie, le 'WIKI' est vide... !");
+            }
+        }
+
 
     };
 
--- gpapp/views.py
+++ gpapp/views.py
@@ -14,25 +14,33 @@ def index():
 
 @app.route("/index/<reflection>/<question>")
 def answer_gp(reflection, question):
-
+    quotas_api = {"over_quotas": "False"}
     time_reflection = time.sleep(int(reflection))
     parse_answer = question_answer.parser(question = question)
 
     place_id_dict = question_answer.get_place_id_list(address = " ".join(parse_answer))
-    place_id = place_id_dict["candidates"][0]["place_id"]
+    try:
+        place_id = place_id_dict["candidates"][0]["place_id"]
+    except IndexError:
+        quotas_api["over_quotas"] = "True"
+        return {"quotas_api": quotas_api}
 
     answer = {
                 "address": question_answer.get_address(place_id = place_id),
                 "history": question_answer.get_history(search_history = " ".join(parse_answer))
     }
 
-    data_map = {
-                "address": answer["address"]["result"]["formatted_address"],
-                "location": answer["address"]["result"]["geometry"]["location"]
-    }
+    try:
+        data_map = {
+                    "address": answer["address"]["result"]["formatted_address"],
+                    "location": answer["address"]["result"]["geometry"]["location"]
+        }
+    except KeyError:
+        quotas_api["over_quotas"] = "True"
+        return {"quotas_api": quotas_api}
 
     display_map = question_answer.get_map_static(data_map)
+    response = {"answer": answer, "display_map": display_map, "quotas_api": quotas_api}
 
-    return (answer,display_map)
-
+    return response
 
