daf08eeb9f21840755beed1de2f2e5b8236a1ede
Pascal MACCHI <pascal.macchi@gmail.com>
2019-11-21 15:16:00 +0100

    Ajout de fonction display pour eviter repition de code sur le comptage de requette (javascript

--- gpapp/static/js/grandPy.js
+++ gpapp/static/js/grandPy.js
@@ -1,8 +1,37 @@
 
 $(document).ready(function(){
+    function display(response_json){
+
+        var instruction = [
+                $('#ask').hide(),
+                $('#word_of_welcome').hide(),
+                $('#gp_reflection').hide(),
+                $('#comprehension').hide(),
+                $('#gp_reply').show(),
+                $('#answer').show(),
+                $('#other').show()
+        ]
+        instruction.push(
+            $('#address').text("l'adresse se situe : "
+            + JSON.stringify(response_json['answer']['address']['result']['formatted_address'])));
+
+        instruction.push($('#map')[0].src = response_json['display_map']);
+
+        var wiki_answer = response_json['answer']['history'];
+        if (wiki_answer[2][0]){
+            var texte = $('#history').text(JSON.stringify(wiki_answer[2][0]));
+        }
+        else{
+            var texte = $('#history').text('Aie aie aie, le \'WIKI\' est vide... !');
+        }
+
+        instruction.push(texte);
+
+        return instruction
+    };
+
     function answer_gp(response){
         var response_json = JSON.parse(response);
-        console.log(response_json);
         if (response_json["quotas_api"]["nb_response"] == 5){
 
             $("#ask").hide();
@@ -12,9 +41,13 @@ $(document).ready(function(){
             $("#overstrain").show();
             $("#answer").show();
             $("#other").show();
+            $.each(display(response_json), function(index, value){
+                $(value);
+            });
 
         }
-        else if ((response_json["quotas_api"]["over_quotas"] == "True") || (response_json["quotas_api"]["nb_response"] >= 10)){
+        else if ((response_json["quotas_api"]["over_quotas"] == "True") ||
+                (response_json["quotas_api"]["nb_response"] >= 10)){
             $("#gp_reflection").hide();
             $("#ask").hide();
             $("#other").hide();
@@ -27,31 +60,13 @@ $(document).ready(function(){
             $("#other").hide();
             $("#ask").show();
             $("#comprehension").show();
+
         }
         else{
-            $("#ask").hide();
-            $("#word_of_welcome").hide();
-            $("#gp_reflection").hide();
-            $("#comprehension").hide();
-            $("#gp_reply").show();
-            $("#answer").show();
-            $("#other").show();
-
-            $("#address").text("l'adresse se situe : "
-                                +JSON.stringify(response_json["answer"]["address"]["result"]["formatted_address"]));
-            $("#map")[0].src = response_json["display_map"];
-
-            wiki_answer = response_json["answer"]["history"];
-
-            if (wiki_answer[2][0]){
-                $("#history").text(JSON.stringify(wiki_answer[2][0]));
-            }
-            else{
-                $("#history").text("Aie aie aie, le 'WIKI' est vide... !");
-            }
+            $.each(display(response_json), function(index, value){
+                $(value);
+            });
         }
-
-
     };
 
     $("#submit2").submit(function(e){
