65066a829835e299d337c21316178fa40e5952a1
Pascal MACCHI <pascal.macchi@gmail.com>
2020-08-05 14:34:53 +0200

    Ajoute de l'attribut comprehension avec ses methodes Ajou de l'ensemble UNNECESSARY_LIST

--- gpapp/blending.py
+++ gpapp/blending.py
@@ -38,9 +38,9 @@ def str_convers(value):
                            # Script function
                            #=================
 
-#----------------------------
+#============================
 
-#----------------------------
+#============================
 
                            #==============
                            # Script class
@@ -48,10 +48,18 @@ def str_convers(value):
 # Redis server organization
 class Chat:
     """
+        API Private Key and Constants Management :
+            local (development) / external (production)
+                status_env()
+                    - key_data["map"]         ==> KEY_API_MAP / HEROKU_KEY_API_MAP
+                    - key_data["staticMap"]   ==> KEY_API_STATIC_MAP / HEROKU_KEY_API_STATIC_MAP
+                    - key_data["status_prod"] ==> True / False
+
         Constants for processing keywords for Google Map APIs and grandpy's behavior
         according to the content of the user question
-            - DONNEE_CIVILITY----set()
-            - INDECENCY_LIST-----set()
+            - CIVILITY_LIST-----|
+            - INDECENCY_LIST----|--set()
+            - UNNECESSARY_LIST--|
 
         Management for initializing configuration database Redis
             - redis_connect() ==> connection initialization for the Redis database
@@ -60,17 +68,18 @@ class Chat:
             - expiry()        ==> data value expiration times for the Redis database
             - reading         ==> read data value for the Redis database
     """
-    #------------------------
+    #========================
     # Data for test civility
-    #------------------------
+    #========================
     DONNEE_CIVILITY = set(
         [
         "bonjour", "bonsoir","salut","hello","hi"
         ]
     )
-    #-----------------------
+
+    #=======================
     # Data for test decency
-    #-----------------------
+    #=======================
     INDECENCY_LIST = set(
         [
         "vieux","con","poussierieux","ancetre","demoder","vieillard","senille",
@@ -80,7 +89,92 @@ class Chat:
         ]
     )
 
+    #=================================
+    # Data for parser (answer's user)
+    #=================================
+    UNNECESSARY_LIST = set(
+        [
+        "a","abord","absolument","afin","ah","ai","aie","ailleurs","ainsi","ait",
+        "allaient","allo","allons","allô","alors","ancetre","ancetre demode",
+        "anterieur","anterieure","anterieures","antiquite","apres","après",
+        "arriere rococo","as","assez","attendu","au","aucun","aucune","aujourd",
+        "aujourd'hui","aupres","auquel","aura","auraient","aurait","auront","aussi",
+        "autre","autrefois","autrement","autres","autrui","aux","auxquelles",
+        "auxquels","avaient","avais","avait","avant","avec","avoir","avons","ayant",
+        "b","bah","bas","basee","bat","beau","beaucoup","bien","bigre","bonjour",
+        "bonjour grandpy","bonjour grandPy\, comment vas tu","bonsoir grandpy","boum",
+        "bravo","brrr","c","car","ce","ceci","cela","celle","celle-ci","celle-là",
+        "celles","celles-ci","celles-là","celui","celui-ci","celui-là","cent",
+        "centenaire senille","cependant","certain","certaine","certaines","certains",
+        "certes","ces","cet","cette","ceux","ceux-ci","ceux-là","chacun","chacune",
+        "chaque","cher","chers","chez","chiche","chut","chère","chères","ci","cinq",
+        "cinquantaine","cinquante","cinquantième","cinquième","clac","clic","combien",
+        "comme","comment","comment allez vous, grandpy","comparable","comparables",
+        "compris","concernant","contre","couic","crac","d","da","dans","de","debout",
+        "dedans","dehors","deja","delà","depuis","dernier","derniere","derriere",
+        "derrière","des","desormais","desquelles","desquels","dessous","dessus","deux",
+        "deuxième","deuxièmement","devant","devers","devra","different","differentes",
+        "differents","différent","différente","différentes","différents",
+        "dinosaure decrepit","dire","directe","directement","dit","dite","dits",
+        "divers","diverse","diverses","dix","e","effet","egale","egalement","egales",
+        "eh","elle","elle-même","elles","elles-mêmes","en","encore","enfin","entre",
+        "envers","environ","es","est","et","etant","etc","etre","eu","euh","eux",
+        "eux-mêmes","exactement","excepté","extenso","exterieur","f","fais",
+        "faisaient","faisant","fait","façon","feront","fi","flac","floc","font","g",
+        "gens","grandpy","h","ha","hello grandpy","hein","hem","hep","hey","hi","ho",
+        "holà","hop","hormis","hors","hou","houp","hue","hui","huit","huitième","hum",
+        "hurrah","hé","hélas","i","il","ils","importe","j","je","jusqu","jusque",
+        "juste","k","l","la","laisser","laquelle","las","le","lequel","les",
+        "lesquelles","lesquels","leur","leurs","longtemps","lors","lorsque","lui",
+        "lui-meme","lui-même","là","lès","m","ma","maint","maintenant","mais","malgre",
+        "malgré","maximale","me","meme","memes","merci","mes","mien","m'indiquer",
+        "m'orienter","mienne","miennes","miens","mille","mince","minimale","moi",
+        "moi-meme","moi-même","moindres","moins","mon","moyennant","multiple",
+        "multiples","même","mêmes","n","na","naturel","naturelle","naturelles","ne",
+        "neanmoins","necessaire","necessairement","neuf","neuvième","ni","nombreuses",
+        "nombreux","non","nos","notamment","notre","nous","nous-mêmes","nouveau","nul",
+        "néanmoins","nôtre","nôtres","o","oh","ohé","ollé","olé","on","ont","onze",
+        "onzième","ore","ou","ouf","ouias","oust","ouste","outre","ouvert","ouverte",
+        "ouverts","o|","où","p","paf","pan","papi","papy","par","parce","parfois",
+        "parle","parlent","parler","parmi", "parseme","partant","particulier",
+        "particulière","particulièrement","pas","passé","pendant","pense","permet",
+        "personne","peu","peut","peuvent","peux","pff","pfft","pfut","pif","pire",
+        "plein","plouf","plus","plusieurs","plutôt","possessif","possessifs",
+        "possible","possibles","pouah","pour","pourquoi","pourrais","pourrait",
+        "pouvait","prealable","precisement","premier","première","premièrement","pres",
+        "probable","probante","procedant","proche","près","psitt","pu","puis",
+        "puisque","pur","pure","q","qu","quand","quant","quant-à-soi","quanta",
+        "quarante","quatorze","quatre","quatre-vingt","quatrième","quatrièmement",
+        "que","quel","quelconque","quelle","quelles","quelqu'un","quelque","quelques",
+        "quels","qui","quiconque","quinze","quoi","quoique","r","rare","rarement",
+        "rares","relative","relativement","remarquable","rend","rendre","restant",
+        "reste","restent","restrictif","retour","revoici","revoilà","rien","s","sa",
+        "sacrebleu","sait","salut","salut grandpy, comment ca va","sans","sapristi",
+        "sauf","se","sein","seize","selon","semblable","semblaient","semble",
+        "semblent","sent","sept","septième","sera","seraient","serait","seront","ses",
+        "seul","seule","seulement","si","sien","sienne","siennes","siens","sinon",
+        "situe","situé","six","sixième","soi","soi-même","soit","soixante","son",
+        "sont","sous","souvent","specifique","specifiques","speculatif","stop",
+        "strictement","subtiles","suffisant","suffisante","suffit","suis","suit",
+        "suivant","suivante","suivantes","suivants","suivre","superpose","sur",
+        "surtout","t","ta","tac","tant","tardive","te","tel","telle","tellement",
+        "telles","tels","tenant","tend","tenir","tente","tes","tic","tien","tienne",
+        "tiennes","tiens","toc","toi","toi-même","ton","touchant","toujours","tous",
+        "tout","toute","toutefois","toutes","treize","trente","tres","trois",
+        "troisième","troisièmement","trop","trouve","très","tsoin","tsouin","tu","té",
+        "u","un","une","unes","uniformement","unique","uniques","uns","v","va","vais",
+        "vas","vers","via","vieillard senille","vieille baderne","vieillot archaique",
+        "vieux","vieux croulant","vieux fossile","vieux gateux","vieux poussierieux",
+        "vif","vifs","vingt","vivat","vive","vives","vlan","voici","voilà","vont",
+        "vos","votre","vous","vous-mêmes","vu","vé","vôtre","vôtres","w","x","y","z",
+        "zut","à","â","ça","ès","étaient","étais","était","étant","été","être","ô",",",
+        ";",".","?","!"
+        ]
+    )
 
+    #======================
+    # Constructor of class
+    #======================
     def __init__(self):
         """
             constructor
@@ -92,6 +186,7 @@ class Chat:
                 - civility         ==> initialization of civility attribut
                 - quotas           ==> initialisation of quotas attribut
                 - indecency        ==> user`s coarseness (value boolean)
+                - comprehension    ==> understanding of user question
                 - nb_indecency     ==> number of user indecency
                 - nb_request       ==> number of user requests
                 - redis_connect()  ==> initialization of the connection method
@@ -107,6 +202,7 @@ class Chat:
         self.civility = False
         self.quotas = False
         self.indecency = False
+        self.comprehension = True
         self.nb_indecency = 0
         self.nb_request = 0
         self.redis_connect()
@@ -285,7 +381,8 @@ class Chat:
         self.write_civility(False)
         self.write_quotas(False)
         self.write_indecency(False)
-        self.write_counter("0")
+        self.write_comprehension(True)
+        self.write_counter(0)
 
     #==============================================
     # value of data Civility in the Redis database
@@ -334,6 +431,26 @@ class Chat:
             reading of indecency configuration in Redis database
         """
         return str_convers(self.reading("indecency"))
+
+    #===============
+    # comprehension
+    #===============
+    def write_comprehension(self, comprehension):
+        """
+            saving of comprehension configuration in Redis database
+        """
+        self.comprehension = comprehension
+        self.writing(
+            "comprehension",
+            bool_convers(self.comprehension)
+        )
+
+    @property
+    def read_comprehension(self):
+        """
+            reading of comprehension configuration in Redis database
+        """
+        return str_convers(Conversation.reading("comprehension"))
 
     #=================
     # Counter Request
@@ -399,6 +516,26 @@ class Chat:
         )
         self.indecency = result
         # ~ self.write_indecency(self.indecency)
+
+    #=========================
+    # Grandpy incomprehension
+    #=========================
+    def comprehension(self):
+    """
+        modification of attributes decency
+    """
+        # list of words to find in questions
+        list_question = self.question.split()
+        # search comprehension
+        result = bool(
+            [
+            w for w in list_question if w.lower() in self.DONNEE_CIVILITY
+                                    and w.lower() in self.INDECENCY_LIST
+                                    and w.lower() in self.UNNECESSARY_LIST
+            ]
+        )
+        self.comprehension = result
+        # ~ self.write_comprehension(self.comprehension)
 
 #==================
 # script execution
