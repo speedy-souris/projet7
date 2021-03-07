6f4a668acc3e2cdea2df6ab19a9f5ab8f1985cb1
Pascal Macchi <pascal.macchi@gmail.com>
2021-03-07 19:26:50 +0100

    Modification du module googlemapsapi et test_google_api ==> remplacement du module urllib par le module requests

--- gpapp/googlemapsapi.py
+++ gpapp/googlemapsapi.py
@@ -3,140 +3,156 @@
 
 import requests
 
-
-def get_requests():
-    request = {
-        'url1': 'https://maps.googleapis.com/maps/api/place/findplacefromtext/',
-        'url2': 'https://maps.googleapis.com/maps/api/place/details/',
-        'url3': 'https://maps.googleapis.com/maps/api/staticmap',
-        'session': requests.Session()
-    }
-    return request
-
-def get_api_data_placeid(title, key_api):
-    data = {
-        'format': 'json',
-        'key': f'{key_api}',
-        'input': f'{title}',
-        'inputtype': 'textquery'
-    }
-    return data
-
-def get_api_data_address(placeid, key_api):
-    data = {
-        'format': 'json',
-        'key': f'{key_api}',
-        'placeid': f'{placeid}',
-        'fields': 'formatted_address,geometry',
-    }
-    return data
-
-def get_api_data_static(address, localization, key_api):
-    data = {
-        'key': f'{key_api}'
-        'center': f"{address['address']['result']['formatted_address']}",
-        'zoom': '18.5',
-        'size': '600x300',
-        'maptype': 'roadmap',
-        'markers': f"color:red%7Clabel:A%7C{localization['lat']},\
-                   {localisation['lng']}"
-    }
-    return data
-
-def get_url_placeid(address, key_api):
+class ApiGoogleMaps:
     """
-        Google map API place_id search function
-    
-    Resultat Ok
-    {
-       "candidates" : [
-          {
-             "place_id" : "ChIJIZX8lhRu5kcRGwYk8Ce3Vc8"
-          }
-       ],
-       "status" : "OK"
-    }
-    API Key Invalid 
-    {
-       "candidates" : [],
-       "error_message" : "The provided API key is invalid.",
-       "status" : "REQUEST_DENIED"
-    }
-    Key API not allowed
-    {
-        "candidates": [],
-        "error_message": "This API key is not authorized to use this service or API.",
-        "status": "REQUEST_DENIED"
-    }
-    Address Invalid 
-    {
-       "candidates" : [],
-       "status" : "ZERO_RESULTS"
-    }
-    """
-    data = get_api_data_placeid(address, key_api)
-    request_data = get_requests()
-    url_api = request_data['url1']
-    request = request_data['session']
-    placeid_found = request.get(url=url_api, params=data)
-    url_placeid = placeid_found.json()
-    return url_placeid
 
-def get_url_address(placeid, key_api):
     """
-        Google map API address search with place_id function
-        Result OK
-        {
-            'html_attributions': [],
-            'result': {
-                'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
-                'geometry': {
-                    'location': {'lat': 48.8975156, 'lng': 2.3833993},
-                    'viewport': {
-                        'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
-                        'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}}}},
-            'status': 'OK'
+    def __init__(self):
+        self.url_api = ''
+        self.url_params = ''
+
+    @staticmethod
+    def get_api_urls():
+        urls = {
+            'url1': 'https://maps.googleapis.com/maps/api/place/findplacefromtext/',
+            'url2': 'https://maps.googleapis.com/maps/api/place/details/',
+            'url3': 'https://maps.googleapis.com/maps/api/staticmap'
         }
-        API Key Invalid
+        return urls
+
+    @staticmethod
+    def get_api_data_placeid(title, key_api):
+        data = {
+            'format': 'json',
+            'key': key_api,
+            'input': title,
+            'inputtype': 'textquery'
+        }
+        return data
+
+    @staticmethod
+    def get_api_data_address(placeid, key_api):
+        data = {
+            'format': 'json',
+            'key': key_api,
+            'placeid': placeid,
+            'fields': 'formatted_address,geometry',
+        }
+        return data
+
+    @staticmethod
+    def get_api_data_static(address, localization, key_api):
+        markers_data = f"color:red%7Clabel:A%7C{localization['lat']},\
+                       {localisation['lng']}"
+        data = {
+            'key': key_api,
+            'center': address['address']['result']['formatted_address'],
+            'zoom': '18.5',
+            'size': '600x300',
+            'maptype': 'roadmap',
+            'markers': markers_data
+        }
+        return data
+    
+    def get_url_placeid(self, address, key_api):
+        """
+            Google map API place_id search function
+        
+        Resultat Ok
         {
+           "candidates" : [
+              {
+                 "place_id" : "ChIJIZX8lhRu5kcRGwYk8Ce3Vc8"
+              }
+           ],
+           "status" : "OK"
+        }
+        API Key Invalid 
+        {
+           "candidates" : [],
            "error_message" : "The provided API key is invalid.",
-           "html_attributions" : [],
            "status" : "REQUEST_DENIED"
         }
         Key API not allowed
         {
+            "candidates": [],
             "error_message": "This API key is not authorized to use this service or API.",
-            "html_attributions": [],
             "status": "REQUEST_DENIED"
         }
-        Place Id Invalid 
+        Address Invalid 
         {
-           "html_attributions" : [],
-           "status" : "INVALID_REQUEST"
+           "candidates" : [],
+           "status" : "ZERO_RESULTS"
         }
-    """
-    data = get_api_data_address(placeid, key_api)
-    request_data = get_requests()
-    url_api = request_data['url2']
-    request = request_data['session']
-    address_found = request.get(url=url_api, params=data)
-    url_address = address_found.json()
-    return url_address
-
-def get_url_static(address, key_api):
-    """
-        Display of the static map at the user's request
-    """
-    address_data = address['address']['result']['formatted_address']
-    localization = address['address']['result']['geometry']['location']
-    data = get_api_data_static(address_data, localization, key_api)
-    request_data = get_requests()
-    url_api = request_data['url3']
-    request = request_data['session']
-    static_found = request.get(url=url_api, params=data)
-    url_static = static_found.json()
-    return url_static
+        """
+        self.url_params = self.get_api_data_placeid(address, key_api)
+        urls_api = self.get_api_urls()
+        self.url_api = urls_api['url1']
+        request = requests.get(self.url_api, self.url_params)
+        url_placeid = request.json()
+        print(request)
+        return request.json()
+    
+    def get_url_address(self, placeid, key_api):
+        """
+            Google map API address search with place_id function
+            Result OK
+            {
+                'html_attributions': [],
+                'result': {
+                    'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
+                    'geometry': {
+                        'location': {'lat': 48.8975156, 'lng': 2.3833993},
+                        'viewport': {
+                            'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
+                            'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}}}},
+                'status': 'OK'
+            }
+            API Key Invalid
+            {
+               "error_message" : "The provided API key is invalid.",
+               "html_attributions" : [],
+               "status" : "REQUEST_DENIED"
+            }
+            Key API not allowed
+            {
+                "error_message": "This API key is not authorized to use this service or API.",
+                "html_attributions": [],
+                "status": "REQUEST_DENIED"
+            }
+            Place Id Invalid 
+            {
+               "html_attributions" : [],
+               "status" : "INVALID_REQUEST"
+            }
+        """
+        self.url_params = self.get_api_data_address(placeid, key_api)
+        urls_api = self.get_api_urls()
+        self.url_api = urls_api['url2']
+        request = requests.get(self.url_api, self.url_params)
+        url_address = request.json()
+        return url_address
+    
+    def get_url_static(self, address, key_api):
+        """
+            Display of the static map at the user's request
+        """
+        address_data = address['address']['result']['formatted_address']
+        localization = address['address']['result']['geometry']['location']
+        self.url_params = self.get_api_data_static(
+            address_data, localization, key_api
+        )
+        urls_api = self.get_api_urls()
+        self.url_api = urls_api['url3']
+        request = requests.get(self.url_api, self.url_params)
+        url_static = request.json()
+        return url_static
 
 
 if __name__ == '__main__':
-    pass
+    from answersearch import KeyManagement
+
+    keys_api = KeyManagement()
+    key_map = keys_api.get_keys['map']
+    api_google = ApiGoogleMaps()
+    api_google.get_url_placeid('OpenClassrooms', key_map)
--- gpapp/tests/test_google_api.py
+++ gpapp/tests/test_google_api.py
@@ -1,213 +1,266 @@
 #coding:utf-8
 #!/usr/bin/env python
 
-import urllib.request
-from io import BytesIO
-import json
-from .. import googlemapsapi
+import requests
+from ..googlemapsapi import ApiGoogleMaps
 from ..answersearch import KeyManagement
 
+class TestParamsImport:
+    """
+        configuration of imported modules
+    """
+    def __init__(self):
+        self.params = self.test_import_params()
+        self.keys_api = self.params['keys_api_value']
+        self.api_google = self.params['api_module']
 
-def get_api_data():
-    api_key = KeyManagement()
-    data  = {
-        'map_key': api_key.get_keys['map'],
-        'static_key': api_key.get_keys['staticMap'],
-        'bad_key': 0,
-        'url': 'https://maps.googleapis.com/maps/api/staticmap',
-        'position': 'center=',
-        'get_zoom': 'zoom=18.5',
-        'get_size': 'size=600x300',
-        'type_get_map': 'maptype=roadmap',
-        'get_marker': 'markers=color:red%7Clabel:A%7C',
-        'key_get_param': 'key='
-    }
-    return data
+    def test_api_keys(self):
+        api_key = KeyManagement()
+        keys_api_Google_map = {
+            'map_key': api_key.get_keys['map'],
+            'static_key': api_key.get_keys['staticMap'],
+            'bad_key': 0
+        }
+        return keys_api_Google_map
 
-def get_url_static(address, key):
-    data = get_api_data()
-    url_static =\
-        f"{data['url']}?{data['position']}"\
-        f"{address['address']['result']['formatted_address']}"\
-        f"&{data['get_zoom']}&{data['get_size']}&{data['type_get_map']}"\
-        f"&{data['get_marker']}"\
-        f"{address['address']['result']['geometry']['location']['lat']},"\
-        f"{address['address']['result']['geometry']['location']['lng']}&"\
-        f"{data['key_get_param']}{key}"
-    return url_static
+    def test_import_params(self):
+        params = {
+            'api_module': ApiGoogleMaps(),
+            'keys_api_value': self.test_api_keys()
+        }
+        return params
+
+# ~ def get_api_data_static(address, localization, key_api):
+    # ~ data  = {
+        # ~ 'center': f"{address['address']['result']['formatted_address']}",
+        # ~ 'zoom': '18.5',
+        # ~ 'size': '600x300',
+        # ~ 'maptype': 'roadmap',
+        # ~ 'markers': f"color:red%7Clabel:A%7C{localization['lat']},\
+                   # ~ {localisation['lng']}",
+        # ~ 'key': f'{key_api}'
+    # ~ }
+    # ~ return data
+
+# ~ def get_url_static(address, key_api):
+    # ~ address_data = address['address']['result']['formatted_address']
+    # ~ localization = address['address']['result']['geometry']['location']
     
-def get_mockreturn(result):
-    def mockreturn(request):
+    # ~ data = get_api_data_static(address_data, localization, key_api)
+    
+    # ~ url_api = request_data['url']
+    # ~ return url_static
+
+
+
+# google map API test on place id location
+def test_geolocal_id(monkeypatch):
+    """
+        Google Map A.P.I test function that returns a file
+        Json containing the reference ID of the address asked
+    """
+    params = TestParamsImport()
+    demand1 = params.api_google.get_url_placeid(
+        'openClassRooms', params.keys_api['map_key']
+    )
+    demand2 = params.api_google.get_url_placeid(
+        'openClassRooms', params.keys_api['bad_key']
+    )
+    demand3 = params.api_google.get_url_placeid(
+    'openClassRooms', params.keys_api['static_key']
+    )
+    demand4 = params.api_google.get_url_placeid(
+    'rueopenClassRooms', params.keys_api['map_key']
+    )
+    result_place_id1 = {
+        'candidates': [{
+            'place_id': 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8'
+        }],
+        'status' : 'OK'
+    }
+    result_place_id2 = {
+        'candidates' : [],
+        'error_message' : 'The provided API key is invalid.',
+        'status' : 'REQUEST_DENIED'
+    }
+    result_place_id3 = {
+        'candidates' : [],
+        'error_message' : 'This API key is not authorized to use this service or API.',
+        'status' : 'REQUEST_DENIED'
+    }
+    result_place_id4 = {
+        'candidates': [],
+        'status' : 'ZERO_RESULTS'
+    }
+    
+    def mock_get(requests, url_params):
         """
             Mock function on api object
         """
-        return BytesIO(json.dumps(result).encode())
-    return mockreturn
+        class JsonResponse:
+            def json(self):
+                return result_place_id1
+        return JsonResponse()
 
-class TestApiGoogle:
-    """
-        management of APi parameters test 
-    """
-    # google map API test on place id location
-    def test_geolocal_id(self, monkeypatch):
-        """
-            Google Map A.P.I test function that returns a file
-            Json containing the reference ID of the address asked
-        """
-        data = get_api_data()
-        demand1 = googlemapsapi.get_place_id_list(
-            'openClassRooms', data['map_key']
-        )
-        demand2 = googlemapsapi.get_place_id_list(
-            'openClassRooms', data['bad_key']
-        )
-        demand3 = googlemapsapi.get_place_id_list(
-        'openClassRooms', data['static_key']
-        )
-        demand4 = googlemapsapi.get_place_id_list(
-        'rueopenClassRooms', data['map_key']
-        )
-        result_place_id1 = {
-            'candidates': [{
-                'place_id': 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8'
-            }],
-            'status' : 'OK'
-        }
-        result_place_id2 = {
-            'candidates' : [],
-            'error_message' : 'The provided API key is invalid.',
-            'status' : 'REQUEST_DENIED'
-        }
-        result_place_id3 = {
-            'candidates' : [],
-            'error_message' : 'This API key is not authorized to use this service or API.',
-            'status' : 'REQUEST_DENIED'
-        }
-        result_place_id4 = {
-            'candidates': [],
-            'status' : 'ZERO_RESULTS'
-        }
-        mockreturn = get_mockreturn('result_place_id1')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand1 == result_place_id1
-        mockreturn = get_mockreturn('result_place_id2')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand2 == result_place_id2
-        mockreturn = get_mockreturn('result_place_id3')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand3 == result_place_id3
-        mockreturn = get_mockreturn('result_place_id4')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand4 == result_place_id4
+    mockreturn = mock_get('result_place_id1')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand1 == result_place_id1
 
-    # google map API test on address location
-    def test_geolocal_address(self, monkeypatch):
+    def mock_get(requests, url_params):
         """
-            Google Map A.P.I test function that returns a file
-            Json containing the reference of the requested address
+            Mock function on api object
         """
-        data = get_api_data()
-        place_id1 = 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8'
-        place_id2 = 'c8'
+        class JsonResponse:
+            def json(self):
+                return result_place_id2
+        return JsonResponse()
+
+    mockreturn = mock_get('result_place_id2')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand2 == result_place_id2
+
+    def mock_get(requests, url_params):
+        """
+            Mock function on api object
+        """
+        class JsonResponse:
+            def json(self):
+                return result_place_id3
+        return JsonResponse()
         
-        demand1 = googlemapsapi.get_address(place_id1, data['map_key'])
-        demand2 = googlemapsapi.get_address(place_id1, data['bad_key'])
-        demand3 = googlemapsapi.get_address(place_id1, data['static_key'])
-        demand4 = googlemapsapi.get_address(place_id2, data['map_key'])
+    mockreturn = mock_get('result_place_id3')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand3 == result_place_id3
 
-        result_address1 = {
-            'html_attributions': [],
-            'result': {
-                'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
-                'geometry': {
-                    'location': {'lat': 48.8975156, 'lng': 2.3833993},
-                    'viewport': {
-                        'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
-                        'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}
-                    }
+    def mock_get(requests, url_params):
+        """
+            Mock function on api object
+        """
+        class JsonResponse:
+            def json(self):
+                return result_place_id3
+        return JsonResponse()
+        
+    mockreturn = mock_get('result_place_id4')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand4 == result_place_id4
+
+# google map API test on address location
+def test_geolocal_address(monkeypatch):
+    """
+        Google Map A.P.I test function that returns a file
+        Json containing the reference of the requested address
+    """
+    params = TestParamsImport()
+    place_id1 = 'ChIJIZX8lhRu5kcRGwYk8Ce3Vc8'
+    place_id2 = 'c8'
+
+    demand1 = params.api_google.get_url_address(
+        place_id1, params.keys_api['map_key']
+    )
+    demand2 = params.api_google.get_url_address(
+        place_id1, params.keys_api['bad_key']
+    )
+    demand3 = params.api_google.get_url_address(
+        place_id1, params.keys_api['static_key']
+    )
+    demand4 = params.api_google.get_url_address(
+        place_id2, params.keys_api['map_key']
+    )
+
+    result_address1 = {
+        'html_attributions': [],
+        'result': {
+            'formatted_address': '10 Quai de la Charente, 75019 Paris, France',
+            'geometry': {
+                'location': {'lat': 48.8975156, 'lng': 2.3833993},
+                'viewport': {
+                    'northeast': {'lat': 48.89886618029151, 'lng': 2.384755530291502},
+                    'southwest': {'lat': 48.89616821970851, 'lng': 2.382057569708498}
                 }
-            },
-            'status': 'OK'
-        }
-        result_address2 = {
-            'error_message' : 'The provided API key is invalid.',
-           'html_attributions' : [],
-           'status' : 'REQUEST_DENIED'
-        }
-        result_address3 = {
-            'error_message' : 'This API key is not authorized to use this service or API.',
-           'html_attributions' : [],
-           'status' : 'REQUEST_DENIED'
-        }
-        result_address4 = {
-            'html_attributions': [],
-            'status': 'INVALID_REQUEST'
-        }
-        mockreturn = get_mockreturn('result_address1')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand1 == result_address1
-        
-        mockreturn = get_mockreturn('result_address2')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand2 == result_address2
+            }
+        },
+        'status': 'OK'
+    }
+    result_address2 = {
+        'error_message' : 'The provided API key is invalid.',
+       'html_attributions' : [],
+       'status' : 'REQUEST_DENIED'
+    }
+    result_address3 = {
+        'error_message' : 'This API key is not authorized to use this service or API.',
+       'html_attributions' : [],
+       'status' : 'REQUEST_DENIED'
+    }
+    result_address4 = {
+        'html_attributions': [],
+        'status': 'INVALID_REQUEST'
+    }
+    mockreturn = mockreturn('result_address1')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand1 == result_address1
+    
+    mockreturn = mockreturn('result_address2')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand2 == result_address2
 
-        mockreturn = get_mockreturn('result_address3')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand3 == result_address3
+    mockreturn = mockreturn('result_address3')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand3 == result_address3
 
-        mockreturn = get_mockreturn('result_address4')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand4 == result_address4
+    mockreturn = mockreturn('result_address4')
+    monkeypatch.setattr(requests.get, mockreturn)
+    assert demand4 == result_address4
 
     # google map API test on map static
-    def test_geolocal_static(self, monkeypatch):
-        """
-            Google Map A.P.I test function that returns a image static
-            containing the reference ID of the address asked
-        """
-        data = get_api_data()
-        address1 = {
-            'address': {
-                'result': {
-                    'formatted_address': '10QuaidelaCharente,75019Paris,France',
-                    'geometry': {
-                        'location': {'lat': 48.8975156, 'lng': 2.3833993}
-                    }
-                }
-            }
-        }
-        address2 = {
-            'address': {
-                'result': {
-                    'formatted_address': 'rueopenClassRooms',
-                    'geometry': {
-                        'location': {'lat': 48.8975156, 'lng': 2.3833993}
-                    }
-                }
-            }
-        }
-        demand1 = googlemapsapi.get_static(address1, data['static_key'])
-        demand2 = googlemapsapi.get_static(address1, data['bad_key'])
-        demand3 = googlemapsapi.get_static(address1, data['map_key'])
-        demand4 = googlemapsapi.get_static(address2, data['static_key'])
+    # ~ def test_geolocal_static(self, monkeypatch):
+        # ~ """
+            # ~ Google Map A.P.I test function that returns a image static
+            # ~ containing the reference ID of the address asked
+        # ~ """
+        # ~ data = get_requests()
+        # ~ address1 = {
+            # ~ 'address': {
+                # ~ 'result': {
+                    # ~ 'formatted_address': '10QuaidelaCharente,75019Paris,France',
+                    # ~ 'geometry': {
+                        # ~ 'location': {'lat': 48.8975156, 'lng': 2.3833993}
+                    # ~ }
+                # ~ }
+            # ~ }
+        # ~ }
+        # ~ address2 = {
+            # ~ 'address': {
+                # ~ 'result': {
+                    # ~ 'formatted_address': 'rueopenClassRooms',
+                    # ~ 'geometry': {
+                        # ~ 'location': {'lat': 48.8975156, 'lng': 2.3833993}
+                    # ~ }
+                # ~ }
+            # ~ }
+        # ~ }
+        # ~ demand1 = googlemapsapi.get_url_static(address1, data['static_key'])
+        # ~ demand2 = googlemapsapi.get_url_static(address1, data['bad_key'])
+        # ~ demand3 = googlemapsapi.get_url_static(address1, data['map_key'])
+        # ~ demand4 = googlemapsapi.get_url_static(address2, data['static_key'])
 
-        result_static1 = get_url_static(address1, data['static_key'])
-        result_static2 = get_url_static(address1, data['bad_key'])
-        result_static3 = get_url_static(address1, data['map_key'])
-        result_static4 = get_url_static(address2, data['static_key'])
+        # ~ result_static1 = get_url_static(address1, data['static_key'])
+        # ~ result_static2 = get_url_static(address1, data['bad_key'])
+        # ~ result_static3 = get_url_static(address1, data['map_key'])
+        # ~ result_static4 = get_url_static(address2, data['static_key'])
 
-        mockreturn = get_mockreturn('result_static1')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand1 == result_static1
-        mockreturn = get_mockreturn('result_static2')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand2 == result_static2
-        mockreturn = get_mockreturn('result_static3')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand3 == result_static3
-        mockreturn = get_mockreturn('result_static4')
-        monkeypatch.setattr(urllib.request, 'urlopen', mockreturn)
-        assert demand4 == result_static4
+        # ~ mockreturn = get_mockreturn('result_static1')
+        # ~ monkeypatch.setattr(requests.get, mockreturn)
+        # ~ assert demand1 == result_static1
+        # ~ mockreturn = get_mockreturn('result_static2')
+        # ~ monkeypatch.setattr(requests.get, mockreturn)
+        # ~ assert demand2 == result_static2
+        # ~ mockreturn = get_mockreturn('result_static3')
+        # ~ monkeypatch.setattr(requests.get, mockreturn)
+        # ~ assert demand3 == result_static3
+        # ~ mockreturn = get_mockreturn('result_static4')
+        # ~ monkeypatch.setattr(requests.get, mockreturn)
+        # ~ assert demand4 == result_static4
 
 
 if __name__ == "__main__":
